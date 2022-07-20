e84ba03125f53e49022a373e9917889af77e4a11
Mshoaib41 <shoaibbabar9612@gmail.com>
2022-07-20 23:43:16 +0500

    ios firebase

--- ios/Sayf/AppDelegate.mm
+++ ios/Sayf/AppDelegate.mm
@@ -1,6 +1,6 @@
 #import "AppDelegate.h"
+#import <Firebase.h>
 #import <GoogleMaps/GoogleMaps.h>
-
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
@@ -29,6 +29,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+[FIRApp configure];
 [GMSServices provideAPIKey:@"AIzaSyAnTW8ZFxothyDHhqgojMm1TFlc0H3wVAU"];
   RCTAppSetupPrepareApp(application);
 
